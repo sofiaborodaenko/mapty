e4f38b2bdf4d9ae752e2159c27c8947b1cc4ac96
Sofia Borodaenko <soph.bor06@gmail.com>
2025-06-09 18:00:59 -0400

    refractored the code into the main App class

--- docs/script.js
+++ docs/script.js
@@ -11,9 +11,37 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-// geolocation api, fist function is when successful other when not
-navigator.geolocation.getCurrentPosition(
-  function (position) {
+// Main class app that handles the loading of the map, position-coordinates, form, displaying markers
+class App {
+  #map;
+  #mapEvent;
+
+  constructor() {
+    // gets the users position on load
+    this._getPosition();
+
+    // once the form is submitted
+    form.addEventListener("submit", this._newWorkout.bind(this));
+
+    // toggle the input type
+    inputType.addEventListener("change", this._toggleElevationField);
+  }
+
+  // gets the position of the user
+  _getPosition() {
+    if (navigator.geolocation) {
+      // geolocation api, fist function is when successful other when not
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("Could not get your position!");
+        }
+      );
+    }
+  }
+
+  // loads the map
+  _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     console.log(latitude, longitude);
@@ -21,19 +49,61 @@ navigator.geolocation.getCurrentPosition(
 
     const coords = [latitude, longitude];
 
-    const map = L.map("map").setView(coords, 13);
+    this.#map = L.map("map").setView(coords, 13);
 
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+    }).addTo(this.#map);
 
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-      .openPopup();
-  },
-  function () {
-    alert("Could not get your position!");
+    // handling clicks on map
+    this.#map.on("click", this._showForm.bind(this));
   }
-);
+
+  // displays the form in the sidebar
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
+
+  // changes one of the inputs if its cycling or running
+  _toggleElevationField() {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  // creates a new workout
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // clear input fields
+    inputDistance.value =
+      inputCadence.value =
+      inputDuration.value =
+      inputElevation.value =
+        "";
+
+    // display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    const clickCoord = [lat, lng];
+
+    L.marker(clickCoord, {
+      riseOnHover: true,
+    })
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 50,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("Workout")
+      .openPopup();
+  }
+}
+
+const app = new App();
